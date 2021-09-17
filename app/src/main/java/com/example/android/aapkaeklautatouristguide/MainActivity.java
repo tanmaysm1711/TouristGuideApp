35ce2d27ddc26b69becd28cfe90a7cfc6fde7711
mayani <arti.agni01@gmail.com>
2021-09-17 22:11:45 +0530

    ui

--- app/src/main/java/com/example/android/aapkaeklautatouristguide/MainActivity.java
+++ app/src/main/java/com/example/android/aapkaeklautatouristguide/MainActivity.java
@@ -5,23 +5,37 @@ import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
     FragmentManager fragmentManager;
     FragmentTransaction fragmentTransaction;
     private String TAG;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         if (savedInstanceState == null) {
             addExploreNearbyFragment();
         }
+
+        drawerLayout=findViewById(R.id.drawer_layout);
+        navigationView =findViewById(R.id.nav_view);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,R.string.open_drawer_content,R.string.close_drawer_content);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
 
         LinearLayout exploreNearbyLayout = (LinearLayout) findViewById(R.id.explore_nearby_layout);
         exploreNearbyLayout.setOnClickListener(new View.OnClickListener() {
